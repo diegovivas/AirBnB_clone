d1f076b822354498cc386120ebf20c7b8525a99d
JUCORTESA <984@holbertonschool.com>
2019-11-13 18:14:43 +0000

    [update] user unittest

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Test of User Class """
 
+from models.base_model import BaseModel
 from models.user import User
 import datetime
 import unittest
@@ -25,6 +26,22 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(self.my_object, 'password'))
         self.assertTrue(hasattr(self.my_object, 'first_name'))
         self.assertTrue(hasattr(self.my_object, 'last_name'))
+
+    def test_hasattr(self):
+        """ attributes inheritated from BaseModel"""
+        self.assertTrue(hasattr(self.my_object, 'name'))
+        self.assertTrue(hasattr(self.my_object, 'id'))
+        self.assertTrue(hasattr(self.my_object, 'created_at'))
+        self.assertTrue(hasattr(self.my_object, 'updated_at'))
+
+    def test_attributes_types(self):
+        """Tests attributes types"""
+        self.assertEqual(type(self.my_object.email), str)
+        self.assertEqual(type(self.my_object.last_name), str)
+        self.assertEqual(type(self.my_object.first_name), str)
+        self.assertEqual(type(self.my_object.password), str)
+        self.assertIsInstance(self.my_object.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_object.updated_at, datetime.datetime)
 
 if __name__ == '__main__':
     unittest.main()

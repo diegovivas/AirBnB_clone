5425dc58425ce9d5f74efd484b436d4b84c7ee83
JUCORTESA <984@holbertonschool.com>
2019-11-14 02:25:06 +0000

    [update] city unittest

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -9,6 +9,7 @@ import unittest
 class TestCity(unittest.TestCase):
     """ Test City Class """
     my_object = City()
+    my_class = City.__dict__
 
     def test_checking_for_docstring_User(self):
         """Test if all docstring were written"""
@@ -22,6 +23,20 @@ class TestCity(unittest.TestCase):
         """ Tests attributes"""
         self.assertEqual(hasattr(self.my_object, "state_id"), True)
         self.assertEqual(hasattr(self.my_object, "name"), True)
+
+    def test_attribute_type(self):
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        my_dict = self.my_object.to_dict()
+        self.assertEqual(self.my_object.__class__.__name__, 'City')
+        self.assertEqual(type(my_dict['created_at']), str)
+        self.assertEqual(type(my_dict['updated_at']), str)
+        self.assertEqual(my_dict["created_at"],
+                         self.my_object.created_at.strftime(t_format))
+        self.assertEqual(my_dict["updated_at"],
+                         self.my_object.updated_at.strftime(t_format))
+        self.assertEqual(type(self.my_class['state_id']), str)
+        self.assertEqual(type(self.my_class['name']), str)
+        
 
 if __name__ == '__main__':
     unittest.main()

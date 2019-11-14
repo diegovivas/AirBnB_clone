60268ec5af8e5902b4a86d724f98739550838be0
JUCORTESA <984@holbertonschool.com>
2019-11-14 02:25:50 +0000

    [update] review unittest

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -9,6 +9,7 @@ import unittest
 class TestReview(unittest.TestCase):
     """ Test Review Class """
     my_object = Review()
+    my_class = Review.__dict__
 
     def test_checking_for_docstring_User(self):
         """Test if all docstring were written"""
@@ -23,6 +24,21 @@ class TestReview(unittest.TestCase):
         self.assertEqual(hasattr(self.my_object, "place_id"), True)
         self.assertEqual(hasattr(self.my_object, "user_id"), True)
         self.assertEqual(hasattr(self.my_object, "text"), True)
+
+    def test_attribute_type(self):
+       t_format = "%Y-%m-%dT%H:%M:%S.%f"
+       my_dict = self.my_object.to_dict()
+       self.assertEqual(self.my_object.__class__.__name__, 'Review')
+       self.assertEqual(type(my_dict['created_at']), str)
+       self.assertEqual(type(my_dict['updated_at']), str)
+       self.assertEqual(my_dict["created_at"],
+                        self.my_object.created_at.strftime(t_format))
+       self.assertEqual(my_dict["updated_at"],
+                        self.my_object.updated_at.strftime(t_format))
+       self.assertEqual(type(self.my_class['place_id']), str)
+       self.assertEqual(type(self.my_class['user_id']), str)
+       self.assertEqual(type(self.my_class['text']), str)
+
 
 if __name__ == '__main__':
     unittest.main()

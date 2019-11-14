bada29fbe90bf10be7112b3ce1dbd31385ceb0fc
JUCORTESA <984@holbertonschool.com>
2019-11-14 02:24:43 +0000

    [update] amenity unittest

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,6 +11,7 @@ import unittest
 class TestAmenity(unittest.TestCase):
     """ Test Amenity Class """
     my_object = Amenity()
+    my_class = Amenity.__dict__
 
     def test_checking_for_docstring(self):
         """Test if all docstring were written"""
@@ -23,6 +24,18 @@ class TestAmenity(unittest.TestCase):
     def test_attribute_name(self):
         """Check attributes"""
         self.assertEqual(hasattr(self.my_object, 'name'), True)
+
+    def test_attribute_type(self):
+       t_format = "%Y-%m-%dT%H:%M:%S.%f"
+       my_dict = self.my_object.to_dict()
+       self.assertEqual(self.my_object.__class__.__name__, 'Amenity')
+       self.assertEqual(type(my_dict['created_at']), str)
+       self.assertEqual(type(my_dict['updated_at']), str)
+       self.assertEqual(my_dict["created_at"],
+                        self.my_object.created_at.strftime(t_format))
+       self.assertEqual(my_dict["updated_at"],
+                        self.my_object.updated_at.strftime(t_format))
+       self.assertEqual(type(self.my_class['name']), str)
 
 if __name__ == '__main__':
     unittest.main()
